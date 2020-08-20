d568d644af877165a0b2cb29d777eb9d144e1851
Akash Das <aakashdas368@gmail.com>
2020-08-20 15:39:46 +0530

    Make InputField functional & add TextField widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:auto_size_text_field/auto_size_text_field.dart';
 import 'package:calculator/styles/styles.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +11,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController _inputController = TextEditingController();
   String _inputText = '';
+  String _diplayInputText = '';
 
   void _updateInputText() {
     this._inputText = _inputController.text;
+    setState(() {
+      _diplayInputText = this._inputText;
+    });
   }
 
-  Widget _buildTextFiled() {
+  Widget _buildInputField() {
     return Container(
       alignment: Alignment.topRight,
-      height: MediaQuery.of(context).size.height * 0.4,
+      height: MediaQuery.of(context).size.height * 0.24,
       width: double.infinity,
       child: SingleChildScrollView(
         child: AutoSizeTextField(
@@ -27,14 +32,30 @@ class _HomeScreenState extends State<HomeScreen> {
             decimal: true,
           ),
           // readOnly: true,
-          maxLines: 8,
+          maxLines: 4,
           minFontSize: 26,
           controller: this._inputController,
           onChanged: (value) => this._updateInputText(),
           style: tnumTextStyle,
           textAlign: TextAlign.end,
+          decoration: InputDecoration(border: InputBorder.none),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField() {
+    return AutoSizeText(
+      this._diplayInputText,
+      style: TextStyle(
+        fontFamily: ttextFamily,
+        fontSize: 28.0,
+        color: Colors.white54,
+      ),
+      textAlign: TextAlign.end,
+      maxLines: 2,
+      minFontSize: 22,
+      overflow: TextOverflow.ellipsis,
     );
   }
 
@@ -49,7 +70,8 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SafeArea(
           child: ListView(
             children: <Widget>[
-              this._buildTextFiled(),
+              this._buildInputField(),
+              this._buildTextField(),
             ],
           ),
         ),
