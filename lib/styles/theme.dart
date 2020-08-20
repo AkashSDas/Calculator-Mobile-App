ee9ae70a5c75e6f05ed9c44ffcf8722666c414ab
Akash Das <aakashdas368@gmail.com>
2020-08-20 12:55:42 +0530

    Add constants in theme.dart

---
+++ lib/styles/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+final tbgPrimary = Color(0xFF070A09);
+final tbgSecondary = Color(0xFF1B1919);
+final ttext1 = Color(0xFFF4F0F0);
+final ttext2 = Color(0xFF0A8449);
+final ttext3 = Color(0xFFB71414);
+
+final tlogoSize = 40.0;
+final ttextFamily = 'Roboto';
+
+final tnumTextStyle = TextStyle(
+  color: ttext1,
+  fontFamily: ttextFamily,
+  fontWeight: FontWeight.w400,
+);
+
+final tcancelTextStyle = TextStyle(
+  color: ttext3,
+  fontFamily: ttextFamily,
+  fontWeight: FontWeight.w400,
+);
