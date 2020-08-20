f1d0fbb637b5465d749d1abd5e1f963fa9b36e37
Akash Das <aakashdas368@gmail.com>
2020-08-20 13:13:07 +0530

    Update logoSize & fontSize

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -6,17 +6,19 @@ final ttext1 = Color(0xFFF4F0F0);
 final ttext2 = Color(0xFF0A8449);
 final ttext3 = Color(0xFFB71414);
 
-final tlogoSize = 40.0;
+final tlogoSize = 30.0;
 final ttextFamily = 'Roboto';
 
 final tnumTextStyle = TextStyle(
   color: ttext1,
   fontFamily: ttextFamily,
   fontWeight: FontWeight.w400,
+  fontSize: 30.0,
 );
 
 final tcancelTextStyle = TextStyle(
   color: ttext3,
   fontFamily: ttextFamily,
   fontWeight: FontWeight.w400,
+  fontSize: 30.0,
 );
